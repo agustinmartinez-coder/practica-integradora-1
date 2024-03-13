0afb8a13f6e7d5eb660ca3f0d4929bbbeb49e0f8
Agustin <am6691944@gmail.com>
2024-03-13 19:26:01 -0300

    first commit

--- src/dao/mongoDB/dbConnection.js
+++ src/dao/mongoDB/dbConnection.js
@@ -2,3 +2,8 @@ import mongoose from "mongoose";
 
 export const connectionToDB = () =>
   mongoose
+    .connect(
+      "[]mongodb+srv://am6591944:Ob90nYqkPCCbOLaV@cluster0.yf0tkel.mongodb.net/"
+    )
+    .then(() => console.log("BBDD Conectada exitosamente"))
+    .catch(() => console.log("Hubo un error en la conexión con la BBDD"));
